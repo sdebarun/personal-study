13da7c26f679174b616f8170a0e27817caae777d
sdebaurn <developer.debarun@gmail.com>
2025-04-18 16:39:28 +0530

    adidng subclass logger as singleton class

--- design-patterns/singleton/Logger.php
+++ design-patterns/singleton/Logger.php
@@ -1,6 +1,34 @@
 <?php
+namespace Designs\php;
+use Designs\php\Singleton;
+
 //lets make a specific subclass - following open closed so the parent class does not need to be modified every time we need a new logic specific singleton class. 
 
+require_once ('index.php');
+
 class Logger extends Singleton {
-    // 
+    // for simplicity we can write log data in a file
+
+    public function writeLog($message) : void {
+        $file = fopen('log.txt', 'a+');
+        fwrite($file, date('y-m-d hh:mm:ss').' : '."$message".PHP_EOL);
+        fclose($file);
+    }
+
+    public static function log($message) :void{
+        $logger = self::instance();
+        $logger->writeLog($message);
+    }
+}
+
+
+Logger::log('Hope log works fine');
+
+$logger1 = Logger::log("I am logger 1");
+$logger2 = Logger::log("I am logger 2");
+
+if($logger1 === $logger2){
+    Logger::log("Logger has only one instance");
+}else{
+    Logger::log("Logger has failed to create a singleton and has two instances");
 }
